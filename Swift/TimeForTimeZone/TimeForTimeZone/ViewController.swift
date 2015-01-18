4919b4bf43f31c3dc993ca93f8d66ab985665885
Roberto Rojas <robertojrojas@gmail.com>
2015-01-17 20:11:53 -0500

    Split date for TimeZone into sections as per design.

--- Swift/TimeForTimeZone/TimeForTimeZone/ViewController.swift
+++ Swift/TimeForTimeZone/TimeForTimeZone/ViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
 
     @IBOutlet weak var timeZonePicker: UIPickerView!
-    @IBOutlet weak var timeForTimeZoneLabel: UILabel!
+    @IBOutlet weak var dateTimeZoneLabel: UILabel!
+    @IBOutlet weak var timeTimeZoneLable: UILabel!
+    @IBOutlet weak var timeZoneLabel: UILabel!
     
     var timeZoneNames = [AnyObject]()
     var timeZones     = [AnyObject]()
+    let dateFormatter = NSDateFormatter()
     
     
     override func viewDidLoad() {
@@ -34,7 +37,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     func populateTimeZones() {
         let abbreviationDictionary = NSTimeZone.abbreviationDictionary()
-        println("available timeZones: \(abbreviationDictionary.count)")
+        //println("available timeZones: \(abbreviationDictionary.count)")
         for (timeZoneName,timeZone) in abbreviationDictionary {
             timeZones.append(timeZone)
             timeZoneNames.append(timeZoneName)
@@ -55,10 +58,32 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     // MARK: UIPickerViewDelegate
     
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 54.0
+    }
+    
+//    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+//        
+//        var outputView = view
+//        
+//        if (outputView == nil) {
+//        
+//            outputView = UIView()
+//            outputView.backgroundColor = UIColor.clearColor()
+//            var label = UILabel()
+//            label.backgroundColor = UIColor.whiteColor()
+//            outputView.addSubview(label)
+//        }
+//        
+//        return outputView
+//        
+//        
+//    }
+    
     func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         
-        let title = NSMutableAttributedString(string: "\(timeZones[row])", attributes: nil)
         
+        let title = NSMutableAttributedString(string: "\(timeZones[row])", attributes: nil)
         return title
      
     }
@@ -69,12 +94,38 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     func displayTimeZoneForSelectedRow(row: Int) {
         let now = NSDate()
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.timeStyle =  NSDateFormatterStyle.FullStyle
-        dateFormatter.dateStyle = .MediumStyle
-        dateFormatter.timeZone = NSTimeZone(abbreviation: timeZoneNames[row] as String)
+        let timeZoneName = timeZoneNames[row] as String
         
-        timeForTimeZoneLabel.text = "\(dateFormatter.stringFromDate(now))"
+        dateTimeZoneLabel.text = extractDate(now, timeZoneName: timeZoneName)
+        timeTimeZoneLable.text = extractTime(now, timeZoneName: timeZoneName)
+        timeZoneLabel.text     = extractTimeZoneName(now, timeZoneName: timeZoneName)
+        
+    }
+    
+    func extractDate(theDate: NSDate, timeZoneName: String) -> String {
+        
+        dateFormatter.timeStyle =  NSDateFormatterStyle.NoStyle
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeZone  = NSTimeZone(abbreviation: timeZoneName)
+        let timeZoneStr:String  = "\(dateFormatter.stringFromDate(theDate))"
+
+        return timeZoneStr
+        
+    }
+    
+    func extractTime(theDate: NSDate, timeZoneName: String) -> String {
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+        dateFormatter.timeZone  = NSTimeZone(abbreviation: timeZoneName)
+        let timeZoneStr:String  = "\(dateFormatter.stringFromDate(theDate))"
+        
+        return timeZoneStr
+    }
+    
+    func extractTimeZoneName(theDate: NSDate, timeZoneName: String) -> String {
+        let timeZone = NSTimeZone(abbreviation: timeZoneName)
+        
+        return timeZone?.name ?? "No TZ"
     }
 
 
